4cf279e1447687db0252978d0cbe4755590e5eb7
ckm67 <77990605+ckm67@users.noreply.github.com>
2023-04-04 09:58:45 -0500

    assignment01

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function printName() {
+    // #console.log('My name is Connor');
+
+    let NBATeams= ["Chicago Bulls", "Cleveland Cavaliers", "Detroit Pistons", "Indiana Pacers"]
+
+    for (let team of NBATeams) {
+        console.log(team);
+      }
+
+    console.log('My name is Connor')
+
+  }
+
+
+  
+
+
+
